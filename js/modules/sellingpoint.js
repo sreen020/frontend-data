eece7e83c64d2e5e7a18ba21097377b3da18d13a
sreen020 <sjoerdreen@gmail.com>
2020-11-27 10:46:14 +0100

    fix bug

--- js/modules/sellingpoint.js
+++ js/modules/sellingpoint.js
@@ -12,123 +12,143 @@ export function showSellingpoint(data) {
 
   // This function places points on the map using lat and long
   function setPoints(data) {
-    const points = g.selectAll("circle").data(data)
-    const totalAmount = d3.select(".totalAmount")
+    const points = g.selectAll("circle").data(data);
+    const totalAmount = d3.select(".totalAmount");
+
+    // update the exsisting points and change the old location to the new location.
+    points
+      .attr("cx", function (d) {
+        return projection([d.location.longitude, d.location.latitude])[0];
+      })
+      .attr("cy", function (d) {
+        return projection([d.location.longitude, d.location.latitude])[1];
+      });
 
     // circle element is being created for each data object, with cx and cy attributes the circles are placed correctly
-    points.enter()
-        .append("circle")
-        .attr("r", 3)
-        .attr("cx", function(d) { return projection([d.location.longitude, d.location.latitude])[0] })
-        .attr("cy", function(d) { return projection([d.location.longitude, d.location.latitude])[1] })
-        .on("mouseover", mouseOver)
-        .on("mousemove", mouseMove)
-        .on("mouseout", mouseOut)
+    points
+      .enter()
+      .append("circle")
+      .attr("r", 3)
+      .attr("cx", function (d) {
+        return projection([d.location.longitude, d.location.latitude])[0];
+      })
+      .attr("cy", function (d) {
+        return projection([d.location.longitude, d.location.latitude])[1];
+      })
+      .on("mouseover", mouseOver)
+      .on("mousemove", mouseMove)
+      .on("mouseout", mouseOut);
 
     // removes circles for the filter options
-    points.exit()
-          .remove()
+    points.exit().remove();
 
     // shows a text with the current amount of circles showed on the map
-    totalAmount
-      .text("Aantal getoonde parkeerautomaten: " + data.length)
+    totalAmount.text("Aantal getoonde parkeerautomaten: " + data.length);
   }
 
   // create the tooltip with styling
-  const tooltip = d3.select("body")
+  const tooltip = d3
+    .select("body")
     .append("div")
     .attr("class", "tooltip")
     .style("position", "absolute")
     .style("z-index", "10")
-    .style("opacity", 1)
+    .style("opacity", 1);
 
-  // On mouseOver show tooltip and input tekst 
+  // On mouseOver show tooltip and input tekst
   function mouseOver(d, i) {
     tooltip
       .append("p")
-        .text(i.sellingpointdesc)
-        .style("opacity", 1.0)
-        .attr("class", "title")
+      .text(i.sellingpointdesc)
+      .style("opacity", 1.0)
+      .attr("class", "title");
   }
 
   // This function changes position of the tooltip to the specific point
   function mouseMove(event) {
     tooltip
-      .style("top", (event.pageY-10)+"px")
-      .style("left",(event.pageX+10)+"px")
-      .style("opacity", 1.0)
+      .style("top", event.pageY - 10 + "px")
+      .style("left", event.pageX + 10 + "px")
+      .style("opacity", 1.0);
   }
 
   // hide tooltip
   function mouseOut(d, i) {
-    tooltip
-      .style("opacity", 0)
-      .selectAll("p").remove();
+    tooltip.style("opacity", 0).selectAll("p").remove();
   }
 
-  //FILTER 
+  //FILTER
   function filterPointsByYears(data) {
-    const resetBtn = d3.select(".reset-button")
+    const resetBtn = d3.select(".reset-button");
 
     // array with all unfiltered years
     const arrayWithAllYears = [];
 
     // The array above get filled with all years
-    data.forEach(element => {
-      arrayWithAllYears.push(element.startdatesellingpoint ? element.startdatesellingpoint.slice(0, 4) : null);
+    data.forEach((element) => {
+      arrayWithAllYears.push(
+        element.startdatesellingpoint
+          ? element.startdatesellingpoint.slice(0, 4)
+          : null
+      );
     });
 
     // filter out the unique values
     // source: https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
     const makeArrUnique = (value, index, self) => {
       if (value != null) {
-        return self.indexOf(value) === index
+        return self.indexOf(value) === index;
       }
-    }
+    };
 
     // Use the func above to filter out all the unique years and order from low to high (ascending)
-    const filteredUniqueValues = arrayWithAllYears.filter(makeArrUnique)
-    filteredUniqueValues.sort(d3.ascending)
+    const filteredUniqueValues = arrayWithAllYears.filter(makeArrUnique);
+    filteredUniqueValues.sort(d3.ascending);
 
     // Make a div inside the form element for each year
-    const form = d3.select("form")
-    .selectAll("div")
+    const form = d3
+      .select("form")
+      .selectAll("div")
       .data(filteredUniqueValues)
       .enter()
-    .append("div")
-      .attr("class", "radioBtn")
-    
+      .append("div")
+      .attr("class", "radioBtn");
+
     // inside the div make an input with the id of the year array. If label get clicked the function changeyear will be triggered
-    form.append("input")
+    form
+      .append("input")
       .attr("type", "radio")
       .attr("name", "radio")
-      .attr("id", (d,i) => filteredUniqueValues[i])
+      .attr("id", (d, i) => filteredUniqueValues[i])
       .on("change", (d, i) => {
         changeYear(i);
-      })
-    
+      });
+
     // inside the div make a label with the text of the specific year
-    form.append("label")
-      .attr("for", (d,i) => filteredUniqueValues[i])
-      .text((d,i) => filteredUniqueValues[i])
+    form
+      .append("label")
+      .attr("for", (d, i) => filteredUniqueValues[i])
+      .text((d, i) => filteredUniqueValues[i]);
 
     // i = the year being clicked. this function updates the points on the map with the point of the chosen year
     function changeYear(i) {
-      const filteredYear = data.filter(row => row.startdatesellingpoint ? row.startdatesellingpoint.slice(0, 4) == i : null)
+      const filteredYear = data.filter((row) =>
+        row.startdatesellingpoint
+          ? row.startdatesellingpoint.slice(0, 4) == i
+          : null
+      );
       setPoints(filteredYear);
     }
 
     // removes chosen filter and uncheck radiobuttons
-    resetBtn
-      .on("click", function() {
-        setPoints(data)
-        uncheckAllRadio()
-      })
+    resetBtn.on("click", function () {
+      setPoints(data);
+      uncheckAllRadio();
+    });
 
     function uncheckAllRadio() {
-      const radioBtns = d3.selectAll("input[type=radio]")
-      radioBtns
-        .property("checked", false)
-    } 
+      const radioBtns = d3.selectAll("input[type=radio]");
+      radioBtns.property("checked", false);
+    }
   }
 }
